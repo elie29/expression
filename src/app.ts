d4fcff24ed5e70b96ab53fededcd316cea679538
Elie NEHME <elie29@gmail.com>
2018-11-11 11:44:11 +0100

    added expression class

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,22 @@
-console.log('1 + 6 + 5 * 8', '=', 1 + 6 + 5 * 8);
-console.log('1 + (6 + 5) * 8', '=', 1 + (6 + 5) * 8);
-console.log('(1 + 6 + 5) * 8', '=', (1 + 6 + 5) * 8);
+import { Expression } from './expression/expression';
+
+{
+  const leftExpression = new Expression(1, '+', 6);
+  const rightExpression = new Expression(5, '*', 8);
+  const expression = new Expression(leftExpression, '+', rightExpression);
+  console.log('1 + 6 + 5 * 8', '=', expression.evaluate());
+}
+
+{
+  const leftExpression = new Expression(6, '+', 5);
+  const rightExpression = new Expression(leftExpression, '*', 8);
+  const expression = new Expression(1, '+', rightExpression);
+  console.log('1 + (6 + 5) * 8', '=', expression.evaluate());
+}
+
+{
+  const firstExpression = new Expression(1, '+', 6);
+  const leftExpression = new Expression(firstExpression, '+', 5);
+  const expression = new Expression(leftExpression, '*', 8);
+  console.log('(1 + 6 + 5) * 8', '=', expression.evaluate());
+}
--- tests/expressions-test.ts
+++ tests/expressions-test.ts
@@ -1,13 +1,24 @@
+import { Expression } from '../src/expression/expression';
+
 describe('Expressions with operator precedence', () => {
   test('1 + 6 + 5 * 8 should return 47', () => {
-    expect(1 + 6 + 5 * 8).toBe(47);
+    const leftExpression = new Expression(1, '+', 6);
+    const rightExpression = new Expression(5, '*', 8);
+    const expression = new Expression(leftExpression, '+', rightExpression);
+    expect(expression.evaluate()).toBe(47);
   });
 
   test('1 + (6 + 5) * 8 should return 89', () => {
-    expect(1 + (6 + 5) * 8).toBe(89);
+    const leftExpression = new Expression(6, '+', 5);
+    const rightExpression = new Expression(leftExpression, '*', 8);
+    const expression = new Expression(1, '+', rightExpression);
+    expect(expression.evaluate()).toBe(89);
   });
 
   test('(1 + 6 + 5) * 8 should return 96', () => {
-    expect((1 + 6 + 5) * 8).toBe(96);
+    const firstExpression = new Expression(1, '+', 6);
+    const leftExpression = new Expression(firstExpression, '+', 5);
+    const expression = new Expression(leftExpression, '*', 8);
+    expect(expression.evaluate()).toBe(96);
   });
 });
